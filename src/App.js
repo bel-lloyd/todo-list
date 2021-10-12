5f975c14894fbadca369336d1954954c38c32856
Bel Lloyd <bel@zandiconsulting.com>
2021-10-12 19:27:52 +1000

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, {useState} from "react";
+import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/Todoitem";
+import "./App.css";
 
 function App() {
-  const [todos, settodos] = useState([
+  const [todos, setTodos] = useState([
     "Learn about React",
     "Meet friend for lunch",
     "Build really cool todo app",
     "Be awesome",
   ]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, text];
+    setTodos(newTodos);
+  };
+
   return (
-  <div>
+  <div className="app">
+  <div className="todo-list">
   <h1>My todo list</h1>
   {todos.map((todo, index) => (
     <TodoItem todo={todo} key={index} />
   ))}
+  <TodoForm addTodo={addTodo} />
+  </div>
   </div>
   );
 }
